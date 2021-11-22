7bf17269e46021e770715cfcb4516b2268f39bf5
Yoieh <johan.nilsson@nerdsofsweden.se>
2021-11-22 20:08:47 +0100

    0.0.2

--- src/SignalListener.ts
+++ src/SignalListener.ts
@@ -1,4 +1,4 @@
-import { Signal } from "./Signal";
+import { Signal } from './Signal';
 
 export class SignalListener<Listener extends (...args: any[]) => any> {
   private signal: Signal<Listener>;
--- src/Singnal.test.ts
+++ src/Singnal.test.ts
@@ -1,6 +1,6 @@
-import { Signal } from "./Signal";
+import { Signal } from './Signal';
 
-describe(">>> Signal", () => {
+describe('>>> Signal', () => {
   let s: Signal<any>;
 
   beforeEach(() => {
@@ -8,31 +8,31 @@ describe(">>> Signal", () => {
     s.removeAll();
   });
 
-  it("should be defined", () => {
+  it('should be defined', () => {
     expect(s).toBeDefined();
   });
 
-  it("should dispach on dispach", () => {
+  it('should dispach on dispach', () => {
     const spy = jest.fn();
     s.add(spy);
     s.dispatch();
     expect(spy).toHaveBeenCalled();
   });
 
-  it("should add a new listener on add", () => {
+  it('should add a new listener on add', () => {
     const spy = jest.fn();
     s.add(spy);
     expect(s.listeners.length).toBe(1);
   });
 
-  it("should remove a listener on remove", () => {
+  it('should remove a listener on remove', () => {
     const spy = jest.fn();
     s.add(spy);
     s.remove(spy);
     expect(s.listeners.length).toBe(0);
   });
 
-  it("should remove all listeners on removeAll", () => {
+  it('should remove all listeners on removeAll', () => {
     const spy = jest.fn();
     s.add(spy);
     s.removeAll();
