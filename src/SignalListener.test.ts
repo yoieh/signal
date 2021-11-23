b801ce1a68c30ee6d8f89dac8737673d5fde6c8d
Yoieh <johan.nilsson@nerdsofsweden.se>
2021-11-23 14:27:24 +0100

    added some more tests

--- src/SignalListener.test.ts
+++ src/SignalListener.test.ts
@@ -6,15 +6,20 @@ describe('>>> SignalListener', () => {
 
   beforeEach(() => {
     s = new Signal();
+    expect(s.listeners.length).toBe(0);
   });
 
   it('should trigger listener function', () => {
     const spy = jest.fn();
     const l = new SignalListener(s, spy);
+    expect(s.listeners.length).toBe(1);
+
     s.dispatch();
+
     expect(spy).toHaveBeenCalled();
 
     l.dispose();
+    expect(s.listeners.length).toBe(0);
   });
 
   it('should dispose on dispose', () => {
@@ -25,5 +30,22 @@ describe('>>> SignalListener', () => {
     expect(s.listeners.length).toBe(1);
     l.dispose();
     expect(s.listeners.length).toBe(0);
+  });
+
+  it('should dispatch but not dispose listeners', () => {
+    const spy1 = jest.fn();
+    const l1 = new SignalListener(s, spy1);
+    expect(s.listeners.length).toBe(1);
+
+    const spy2 = jest.fn();
+    new SignalListener(s, spy2);
+    expect(s.listeners.length).toBe(2);
+
+    l1.dispose();
+    expect(s.listeners.length).toBe(1);
+
+    s.dispatch();
+    expect(spy1).toHaveBeenCalledTimes(0);
+    expect(spy2).toHaveBeenCalled();
   });
 });
--- src/Singnal.test.ts
+++ src/Singnal.test.ts
@@ -5,14 +5,14 @@ describe('>>> Signal', () => {
 
   beforeEach(() => {
     s = new Signal();
-    s.removeAll();
+    expect(s.listeners.length).toBe(0);
   });
 
   it('should be defined', () => {
     expect(s).toBeDefined();
   });
 
-  it('should dispach on dispach', () => {
+  it('should dispatch on dispatch', () => {
     const spy = jest.fn();
     s.add(spy);
     s.dispatch();
@@ -28,13 +28,35 @@ describe('>>> Signal', () => {
   it('should remove a listener on remove', () => {
     const spy = jest.fn();
     s.add(spy);
+    expect(s.listeners.length).toBe(1);
     s.remove(spy);
     expect(s.listeners.length).toBe(0);
   });
 
+  it('should dispatch but not removed listeners', () => {
+    const spy1 = jest.fn();
+    const spy2 = jest.fn();
+
+    s.add(spy1);
+    expect(s.listeners.length).toBe(1);
+
+    s.add(spy2);
+    expect(s.listeners.length).toBe(2);
+
+    s.remove(spy1);
+    expect(s.listeners.length).toBe(1);
+
+    s.dispatch();
+    expect(spy1).toHaveBeenCalledTimes(0);
+    expect(spy2).toHaveBeenCalled();
+  });
+
   it('should remove all listeners on removeAll', () => {
     const spy = jest.fn();
-    s.add(spy);
+    const count = 5;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    [...Array(count)].forEach(() => s.add(spy));
+    expect(s.listeners.length).toBe(count);
     s.removeAll();
     expect(s.listeners.length).toBe(0);
   });
